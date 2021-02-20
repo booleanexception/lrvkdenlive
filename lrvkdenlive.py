d57f2944cbf006b24c30d413b2760ccd6de8e2cb
basel <bauermann74@gmail.com>
2021-02-20 07:37:10 +0100

    subfolder support and cleaner output

--- lrvkdenlive.py
+++ lrvkdenlive.py
@@ -19,41 +19,41 @@ if len(sys.argv) != 2:
 
 from_path = sys.argv[1]
 
-for file in os.listdir(from_path):
-    if not file.endswith(".LRV"):
-        print("[0.0] "+file+" is no LRV")
-        continue
 
-    lrv_file = from_path+file
-    mp4_file = from_path+file[:-3]+"MP4"
+for path, subdirs, files in os.walk(from_path):
+    for file in files:
 
-    if not os.path.isfile(mp4_file):
-        print("[0.1] "+mp4_file+" does not exist")
-        continue
+        if not file.endswith(".LRV"):
+            continue
 
-    filesize = os.stat(mp4_file).st_size
+        lrv_file = path+"/"+file
+        mp4_file = path+"/"+(file[:-3]+"MP4")
 
-    if filesize <= 2000000:
-        print("[0.2] "+mp4_file+" is too small")
-        continue
+        if not os.path.isfile(mp4_file):
+            print("[0.1] found LRV, but "+mp4_file+" does not exist in same folder?")
+            continue
 
-    with open(mp4_file, "rb") as f:
-        fdata = f.read(1000000)
-        
-        f.seek(filesize - 1000000)
-        fdata += f.read(1000000)
-        
-        result = hashlib.md5(fdata)
+        filesize = os.stat(mp4_file).st_size
 
-    hash = result.hexdigest()
+        if filesize <= 2000000:
+            print("[0.2] "+mp4_file+" is too small, skipping")
+            continue
 
-    print("[1] "+mp4_file+" has hash: "+hash)
+        with open(mp4_file, "rb") as f:
+            fdata = f.read(1000000)
+            
+            f.seek(filesize - 1000000)
+            fdata += f.read(1000000)
+            
+            result = hashlib.md5(fdata)
 
-    proxy_file = to_path+"/"+hash+".mkv"
+        hash = result.hexdigest()
 
-    if os.path.isfile(proxy_file):
-        print("[1.1] "+mp4_file+" already exists")
-        continue
+        proxy_file = to_path+"/"+hash+".mkv"
 
-    print("[2] >>> Copying "+lrv_file+" -> "+proxy_file)
-    shutil.copy2(lrv_file, proxy_file)
+        if os.path.isfile(proxy_file):
+            print("[1.1] "+mp4_file+" already proxied")
+            continue
+
+        print("[2] >>> Copying "+lrv_file+" -> "+proxy_file)
+        shutil.copy2(lrv_file, proxy_file)
